badc0f03e94679a144f666a77a04f5527235db65
siwarbenrhouma <benrhoumamohamed752@gmail.com>
2022-08-10 14:45:52 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -3,12 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import BasicExample from './form.js'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BasicExample/>
 );
 
 // If you want to start measuring performance in your app, pass a function
